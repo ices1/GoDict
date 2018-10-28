27de1d85ede6d5a680b4a58e8228847f6173f24d
ices1 <iceesong@outlook.com>
2018-10-28 17:36:52 +0800

    up app.js :star:

--- app.js
+++ app.js
@@ -2,63 +2,65 @@ const axios = require('axios')
 const wordBaseUrl = 'http://104.194.71.73:5001/word/'
 const AutoBaseUrl = 'http://104.194.71.73:5001/autocomplete/'
 
-// let options = {
-//   hostname: '127.0.0.1',
-//   port: 8080,
-//   path: '/http://4.cddm.me:5001/word/love'
-// };
-
-
-const readline = require('readline');
+const readline = require('readline')
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
   prompt: 'GoDict >>> '
-});
+})
 
-rl.prompt();
+rl.prompt()
 
-function getAutoWord(word) {
-  console.log('未查询到准确单词, 以下为相关联想词')
-  axios.get(AutoBaseUrl + word)
-    .then((res) => 
-      // res.data.results.forEach((it,cur) => console.log(it.searchtext))
-      console.log(res.data.results.reduce((pre,cur) => pre +=cur.searchtext + ', ', ''))
-    )
+const getAutoWord = async (word) => {
+  await axios.get(AutoBaseUrl + word)
+    .then((res) => {
+      let ary = res.data.results
+      if (ary.length) {
+        console.log(`\n<<< msg: 未查询到 '${word}', 以下为联想词  ^_^`)
+        ary.forEach((it,idx) => console.log(`  ${idx}: ${it.searchtext}`))
+        // console.log('联想词: ', ary.reduce((pre,cur) => pre +=cur.searchtext + ', ', ''))
+      } else {
+        console.log(`\n<<< msg: 抱歉，这里没有你要的答案哦 *_*`)
+      }
+    })
     .catch((err) => {
       console.log(err)
     })
 }
 
+const getWord = async (word) => {
+  await axios.get(wordBaseUrl + word)
+      .then(async res => {
+        let data = res.data
+        // 未查询到准确单词
+        if (data.length === 0) {
+          await getAutoWord(word)
+        } else {
+          data.forEach((it, idx) => {
+            console.log( '\n# ', idx + 1)
+            let info = it.senses[0].defs[0]
+            console.log('Cn: ', info.defCn.replace(/\s+/g,' '))
+            console.log('En: ', info.defEn.replace(/\s+/g,' '))
+            console.log('example: ', '\n', '  ' + info.examples[0].cn.replace(/\s+/g,' '),
+              '\n', '  ' + info.examples[0].en.replace(/\s+/g,' '))
+          })
+        }
+      })
+      .catch(err => console.log(err))
+  }
+
 rl.on('line', async (line) => {
+  if (line.trim() === '') {
+    return rl.prompt()
+  }
 
-  // 开始请求数据
-  await axios.get(wordBaseUrl + line.trim())
-    .then(res => {
-      let data = res.data
+  // 请求数据
+  await getWord(line.trim())
+  console.log('\n')
 
-      // 未查询到准确单词
-      if (data.length === 0) {
-        getAutoWord(line.trim())
-        rl.prompt();
-      } else {
-        data.forEach((it, idx) => {
-          console.log( ' #', idx + 1)
-          let info = it.senses[0].defs[0]
-          console.log('Cn: ', info.defCn.replace(/\s+/g,' '))
-          console.log('En: ', info.defEn.replace(/\s+/g,' '))
-          console.log('example: ', '\n', '  ' + info.examples[0].cn.replace(/\s+/g,' '),'\n', '  ' + info.examples[0].en.replace(/\s+/g,' '))
-        })
-        // console.log()
-      }
-    })
-    .catch(err => console.log(err))
-  rl.prompt();
+  rl.prompt()
 }).on('close', () => {
-  console.log('Bye...');
-  process.exit(0);
-});
+  console.log('Bye...')
+  process.exit(0)
+})
 
-
-
-// axios.listen(80, console.log('(GoDict)listen on port 80...'))
